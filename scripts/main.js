cce5724a2bf131dae5db4b85b555bd17e9c2428c
Cyril Lavanchy <lac@BSGROUP.BSGROUP.CH>
2018-10-09 15:08:15 +0200

    Fixed open chart button

--- scripts/main.js
+++ scripts/main.js
@@ -36,6 +36,10 @@ var showChartBtn = document.getElementById("show chart");
 var span = document.getElementByClassName('close')[0];
 //When the user clicks on the button, open the Modal
 showChartBtn.onclick = function() {
+	modal.style.display = "block";
+}
+//When the user clicks on <span> (x), close the Modal
+span.onclick = function() {
 	modal.style.display = "none";
 }
 //When the user clicks anywhere outside of the modal, close it
