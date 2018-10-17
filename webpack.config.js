0da51112c1fc19cd3aa7f20e6b039aa82817513f
Cyril Lavanchy <lac@BSGROUP.BSGROUP.CH>
2018-10-17 09:43:17 +0200

    set up javascript for material design

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const autoprefixer = require('autoprefixer');
 
-module.exports = [{
-  entry: './app.scss',
+module.exports = {
+  entry: ['./app.scss', './scripts/app.js'],
   output: {
-    // This is necessary for webpack to compile
-    // But we never use style-bundle.js
-    filename: 'style-bundle.js',
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -18,22 +16,29 @@ module.exports = [{
               name: 'bundle.css',
             },
           },
-          { loader: 'extract-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'postcss-loader',
+          {loader: 'extract-loader'},
+          {loader: 'css-loader'},
+          {loader: 'postcss-loader',
             options: {
-              plugins: () => [autoprefixer()]
-            }
+              plugins: () => [autoprefixer()],
+            },
           },
           {
             loader: 'sass-loader',
             options: {
-              includePaths: ['./node_modules']
-            }
-          },
-        ]
+              includePaths: ['./node_modules'],
+            },
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-object-assign']
+        },
       }
-    ]
+    ],
   },
-}];
+};
